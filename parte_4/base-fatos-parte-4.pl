908ac3e919638dac3609e5829e2af6abce14db1c
FilipeIzidorio <fmotor312@gmail.com>
2024-12-07 02:31:32 -0300

    video-parte-3

--- parte_4/base-fatos-parte-4.pl
+++ parte_4/base-fatos-parte-4.pl
@@ -1,22 +1,47 @@
-% Relaciona pessoas aos bairros onde moram
-mora(ana, centro).
-mora(bruno, jardim).
-mora(carla, centro).
-mora(daniel, bosque).
-mora(elaine, jardim).
-mora(fernando, bosque).
+% relaciona pessoas aos bairros onde moram
+mora(ana,centro).
+mora(bruno,jardim).
+mora(carla,centro).
+mora(daniel,bosque).
+mora(elaine,jardim).
+mora(fernando,bosque).
 
-% Relaciona bairros às regiões
-pertence(centro, norte).
-pertence(jardim, norte).
-pertence(bosque, sul).
+%relaciona bairros
 
-% Define quem é amigo de quem
-amigo(ana, bruno).
-amigo(bruno, carla).
-amigo(carla, daniel).
-amigo(elaine, fernando).
+pertence(centro,norte).
+pertence(jardim,norte).
+pertence(bosque,sul).
 
-% Define quem tem carro
+%relaciona quem e amigo de quem
+
+amigo(ana,bruno).
+amigo(bruno,carla).
+amigo(carla,daniel).
+amigo(elaine,fernando).
+
+%define quem tem carro
+
 tem_carro(ana).
 tem_carro(daniel).
+
+%Regra para dar carona
+
+pode_dar_carona(Pessoa1, Pessoa2) :-
+    tem_carro(Pessoa1),
+    mora(Pessoa1, Bairro1),
+    mora(Pessoa2, Bairro2),
+    pertence(Bairro1,Regiao),
+    pertence(Bairro2,Regiao),
+    Regiao = cidade_nort.
+
+
+
+
+
+
+
+
+
+
+
+
